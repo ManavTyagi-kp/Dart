86e5de955dd399c3fc8a7cf6edf519659fcacd99
ManavTyagi-kp <tyagikdmanav@gmail.com>
2023-08-22 15:03:47 +0530

    pushing after adding a few more examples

--- bin/api_maintainers.dart
+++ bin/api_maintainers.dart
@@ -1,72 +1,125 @@
-// inherited 🚫
+// inherited 🚫 //can extend in the same library
 // implemented ✅
-// generated ✅
-interface class Toy{
-  String play(String toy){
+// construct ✅
+interface class Toy {
+  String play(String toy) {
     return "I'm playing with $toy";
+  }
+
+  void stopPlay(){
   }
 }
 
-base class BaseClass{
-  void baseMethod([String? name]){
+class InterFuckingFace implements Toy {
+  @override
+  String play(String toy) {
+    // TODO: implement play
+    throw UnimplementedError();
+  }
+  
+  @override
+  void stopPlay() {
+    // TODO: implement stopPlay
+  }
+}
+
+base class BaseClass {
+  void baseMethod([String? name]) {
     print('Hi $name, this is base class');
-  } 
+  }
 }
 
 // inherited ✅
-// implemented 🚫
-// generated ✅
-abstract class Abs{
+// implemented ✅
+// construct 🚫
+abstract class Abs {
   void method(String str);
-  void method2(String lname){
+  void method2(String lname) {
     print('method2');
   }
 }
 
-void main(List<String> args){
+void main(List<String> args) {
+  // *** ERROR ****
   // Abstract classes can't be instantiated.
   // Try creating an instance of a concrete subtype.
   // Seal().sealMethod(name: 'manav');
 
   BaseClass().baseMethod('manav');
 
+  // *** ERROR ****
   // Abstract classes can't be instantiated.
   // Try creating an instance of a concrete subtype.
   // Abs().method('name');
 
   Extender().method('manav');
 
-
+  FinalClass().finalMethod('manav');
 }
 
 // inherited 🚫
 // implemented 🚫
-// generated 🚫
-sealed class Seal{
-  void sealMethod({required String? name}){
+// construct 🚫
+sealed class Seal {
+  void sealMethod({required String? name}) {
     print('Hi $name, this is sealed class');
   }
 }
 
-class Extender extends Abs{
+// inherited ✅
+// implemented ✅
+// construct ✅
+// mix-in ✅
+mixin class Mix {
+  void mixMethod(String name) {
+    print('Hi $name, this is method1 in mixin class');
+  }
+
+  void mixMethod2(String name) {
+    print('Hi $name, this is method2 in mixin class');
+  }
+}
+
+class Mixer with Mix {
+  @override
+  void mixMethod(String name) {
+    // TODO: implement mixMethod
+    super.mixMethod(name);
+  }
+}
+
+// inherited 🚫
+// implemented 🚫
+// construct ✅
+final class FinalClass {
+  void finalMethod(String name) {
+    print('Hi $name, this is a method in final class');
+  }
+}
+
+class Extender implements Abs {
   @override
   void method(String name) {
     print('Hi $name, this is extended class for abstract class');
   }
-}
 
-class Implementer implements Toy{
   @override
-  String play(String toy) {
-    // TODO: implement play
-    throw UnimplementedError();
+  void method2(String lname) {
+    // TODO: implement method2
   }
-
 }
 
+// class Implementer implements Toy {
+//   @override
+//   String play(String toy) {
+//     // TODO: implement play
+//     throw UnimplementedError();
+//   }
+// }
 
+// *** ERROR ****
+// The type 'ExtendFinal' must be 'base', 'final' or 'sealed' 
+// because the supertype 'FinalClass' is 'final'.
+// class ExtendFinal extends FinalClass{
 
-
-
-
-
+// }
--- bin/dart_learn.dart
+++ bin/dart_learn.dart
@@ -2,6 +2,7 @@ import 'dart:mirrors';
 
 import 'package:dart_learn/dart_learn.dart' as dart_learn;
 
+import 'api_maintainers.dart';
 
 // TODO:  calling a class as a function. Important
 
@@ -25,13 +26,13 @@ void main(List<String> arguments) {
   // number with fun1 function
   TypedefSampleFun number = fun1;
   // Calling number
-  number(1,2);
-   
+  number(1, 2);
+
   // Redefining number
   // with fun2 function
   number = fun2;
   // Calling number
-  number(3,4);
+  number(3, 4);
 
   FactoryConst._default();
 
@@ -45,80 +46,77 @@ void main(List<String> arguments) {
   //   list.forEach(printElement);
 }
 
-
-
 class Toppings<t extends Object> {
   Toppings();
-    var toppings = <dynamic>['onion' , 'tomato'];
-    Toppings.topping(this.toppings);    
-  
+  var toppings = <dynamic>['onion', 'tomato'];
+  Toppings.topping(this.toppings);
 }
 
 //CONSTRUCTORS
-  // optional named constructors cannot be private
-  // Fraction({this._numerator, this._denominator}) **Doesn't compile**
-  // above code doesn't compile
-  // Fraction([this._numerator, this._denominator]) **Compiles**
-  // However, you can have private positional parameters
+// optional named constructors cannot be private
+// Fraction({this._numerator, this._denominator}) **Doesn't compile**
+// above code doesn't compile
+// Fraction([this._numerator, this._denominator]) **Compiles**
+// However, you can have private positional parameters
 class Fraction {
   final int _numerator;
   final int _denominator;
   late final double _rational;
 
-  Fraction(this._numerator, this._denominator){
+  Fraction(this._numerator, this._denominator) {
     // Future<double> _rational = Future.delayed(Duration(seconds: 6), (){
     // return _numerator/_denominator;});
 
-    _rational = _numerator/_denominator;
+    _rational = _numerator / _denominator;
   }
 
   // Named Constructors
-    // They are the only alternative to have multiple constructors
-    // since dart has no method overload
-  Fraction.zero() :
-    _numerator = 0,
-    _denominator = 1;
+  // They are the only alternative to have multiple constructors
+  // since dart has no method overload
+  Fraction.zero()
+      : _numerator = 0,
+        _denominator = 1;
 
   // ANOTHER EXAMPLE
-  Fraction.fromDecimal(double decimal) :
-    _numerator = decimal.toInt(),
-    _denominator = 1,
-    _rational = decimal;
+  Fraction.fromDecimal(double decimal)
+      : _numerator = decimal.toInt(),
+        _denominator = 1,
+        _rational = decimal;
 
   // Redirecting constructor
-    // can be used to implement constructors that does the same thing
-    // as another constructor. can be used to avoid code duplication
-  Fraction.oneHalf(): this(1,2);
-  Fraction.whole(int val): this(val, 1);
-    // Fraction.oneHalf is just another way to call Fraction.
-    // But this method avoids code repetion.
-    // This feature can be very useful when combined with named constructors
+  // can be used to implement constructors that does the same thing
+  // as another constructor. can be used to avoid code duplication
+  Fraction.oneHalf() : this(1, 2);
+  Fraction.whole(int val) : this(val, 1);
+  // Fraction.oneHalf is just another way to call Fraction.
+  // But this method avoids code repetion.
+  // This feature can be very useful when combined with named constructors
   // END
 }
 
-class FactoryConst{
+class FactoryConst {
   // Factory Contructor
-    // The "factory" keyword returns an instance of the given class that's not necessarily a new one.
-    // It can be useful when:
-    // > You want to return an instance of a class instead of the class itself
-    // > You want to implement a singleton (the Singleton pattern) //??????????
-    // > you want to return an instance from the cache
+  // The "factory" keyword returns an instance of the given class that's not necessarily a new one.
+  // It can be useful when:
+  // > You want to return an instance of a class instead of the class itself
+  // > You want to implement a singleton (the Singleton pattern) //??????????
+  // > you want to return an instance from the cache
 
-    // Factory constructors are like static methods(???) and so you don't have access to "this"
-    // There cannot be a factory and a "normal" constructor with the same name.
+  // Factory constructors are like static methods(???) and so you don't have access to "this"
+  // There cannot be a factory and a "normal" constructor with the same name.
 
-  var data = <int>[1,2,3,4];
+  var data = <int>[1, 2, 3, 4];
   //static final _objects = List<BigObject>.add("obj", 5, ${data})();
   static final List<BigObject> objects = [];
-  factory FactoryConst(BigObject obj){
-    if(!objects.contains(obj)){
+  factory FactoryConst(BigObject obj) {
+    if (!objects.contains(obj)) {
       objects.add(obj);
     }
 
     return FactoryConst._default();
   }
 
-  FactoryConst._default(){
+  FactoryConst._default() {
     print("Example of Factory Constructor");
   }
 }
@@ -131,25 +129,23 @@ class BigObject {
   BigObject(this.name, this.size, this.data) {
     // Additional initialization or logic can be added here
   }
-  BigObject.add(String name, int size, List<int> data): this(name, size, data);
+  BigObject.add(String name, int size, List<int> data) : this(name, size, data);
 }
 
-
 // initializer list
-  // the following can be used if you wanted to
-  // keep private constructors name hidden, i.e
-  // call them with a different name.
-  // Initializer list are executed before the body
-  // and thus variables are immediately initialized.
-  // No need for nullable types or late.
+// the following can be used if you wanted to
+// keep private constructors name hidden, i.e
+// call them with a different name.
+// Initializer list are executed before the body
+// and thus variables are immediately initialized.
+// No need for nullable types or late.
 class Initializer {
   int _secret;
   double _stillSecret;
 
   Initializer(int age, double money)
-    : _secret = age,
-      _stillSecret = money;
-  
+      : _secret = age,
+        _stillSecret = money;
 }
 
 // CLASS MODIFIERS FOR API MAINTAINERS
@@ -157,27 +153,30 @@ class Initializer {
 // Mixins are a way of defining code that can be reused in multiple class hierarchies
 // "with" keyword is used to use Mixins
 mixin Musical {
-  void genre(){
+  void genre() {
     print("genre presented here");
   }
 }
 
-class Musician with Musical{
-
+class Musician with Musical {
   @override
   void genre() {
     print("genre in mixin");
     usingGenre();
   }
-  void usingGenre(){
+
+  void usingGenre() {
     super.genre();
   }
 }
 
 //Sealed modifier
-sealed class Amigo{}
+sealed class Amigo {}
+
 class Lucky extends Amigo {}
+
 class Dusty extends Amigo {}
+
 class Ned extends Amigo {}
 
 // class SealedExample {
@@ -190,34 +189,30 @@ class Ned extends Amigo {}
 // }
 
 // Dart program to show the usage of typedef
- 
+
 // Defining alias name
 typedef TypedefSampleFun(int a, int b);
- 
+
 // Defining Geek1 function
 fun1(int a, int b) {
   print("This is Geek1");
   print("$a and $b are lucky geek numbers !!");
 }
- 
+
 // Defining Geek2 function
 fun2(int a, int b) {
   print("This is Geek2");
   print("$a + $b is equal to ${a + b}.");
 }
- 
 
-
-
-
-void funVar(){
+void funVar() {
   var value; //this is preferred
   String name; // expilit type casting is not preferred
 
   // var pizza = List<List<Toppings>>();
-  var newList = <int>[1,2,4];
+  var newList = <int>[1, 2, 4];
   var t = Toppings<int>.topping(newList);
-  for(var topping in t.toppings){
+  for (var topping in t.toppings) {
     print('$topping');
   }
 }
